216172c117aec6cf2d7bc6e83303ddfb65a788d9
dufontdd <dufont.dd@gmail.com>
2026-05-12 13:57:17 +0200

    Add complex Express server with routing and async CSV reading

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const fs = require('fs');
+
+const database = process.argv[2];
+
+const countStudents = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
+
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    const students = lines.slice(1);
+
+    let result = `Number of students: ${students.length}`;
+
+    const fields = {};
+    students.forEach((student) => {
+      const [firstname, , , field] = student.split(',');
+      if (!fields[field]) fields[field] = [];
+      fields[field].push(firstname);
+    });
+
+    Object.entries(fields).forEach(([field, names]) => {
+      result += `\nNumber of students in ${field}: ${names.length}. List: ${names.join(', ')}`;
+    });
+
+    resolve(result);
+  });
+});
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  countStudents(database)
+    .then((data) => {
+      res.send(`This is the list of our students\n${data}`);
+    })
+    .catch((err) => {
+      res.send(`This is the list of our students\n${err.message}`);
+    });
+});
+
+app.listen(1245);
+
+module.exports = app;
